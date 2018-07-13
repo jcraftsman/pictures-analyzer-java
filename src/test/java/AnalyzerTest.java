c0e2d7fdc3d38dfc31cc2b464dc0694c1f0f2a7d
Wassel Alazhar <wassel.alazhar@gmail.com>
2018-07-13 07:15:56 +0200

    configure junit 5

---
+++ src/test/java/AnalyzerTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AnalyzerTest {
+
+    @Test
+    void should() {
+        assertThat(false).isTrue();
+    }
+}
