fd6efd402fbd2db662ca4cabf6b99d48ab3b5700
Wassel Alazhar <wassel.alazhar@gmail.com>
2018-08-11 19:06:02 +0200

    refactor: rename acceptance test to IndexPictureContentFeature

--- src/test/java/feature/IndexPictureContentFeature.java
+++ src/test/java/feature/IndexPictureContentFeature.java
@@ -1,4 +1,4 @@
-package acceptance;
+package feature;
 
 import org.evolutionary.Analyzer;
 import org.evolutionary.PictureContent;
@@ -13,10 +13,10 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
 
-class AnalyzerTest {
+class IndexPictureContentFeature {
 
-    private static final ClassLoader CLASS_LOADER = AnalyzerTest.class.getClassLoader();
-    private static final String PICTURES_DIRECTORY_PATH = CLASS_LOADER.getResource("pictures").getFile();
+    private static final ClassLoader CLASS_LOADER = IndexPictureContentFeature.class.getClassLoader();
+    private static final String PICTURES_DIRECTORY_PATH = CLASS_LOADER.getResource("pictures").getPath();
     private static final String PICTURE_FILE_NAME = "top_secret.png";
     private static final String PATH_TO_A_PICTURE_FILE = Paths.get(PICTURES_DIRECTORY_PATH, PICTURE_FILE_NAME).toString();
     private static final String PUBLISHED_PICTURE_URL = "https://s3.eu-west-3.amazonaws.com/evolutionary-confidential/agent-phillip/top_secret.png";
