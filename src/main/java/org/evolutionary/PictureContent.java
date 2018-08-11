bc8417231c4f64e8c728307b8530f0f2bf940cbb
Wassel Alazhar <wassel.alazhar@gmail.com>
2018-08-11 12:28:09 +0200

    Use lombok to simplify value objects and DTOs implementation (less verbosity)

--- src/main/java/org/evolutionary/PictureContent.java
+++ src/main/java/org/evolutionary/PictureContent.java
@@ -1,7 +1,8 @@
 package org.evolutionary;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class PictureContent {
 
     private final String name;
@@ -12,30 +13,5 @@ public class PictureContent {
         this.name = name;
         this.url = url;
         this.description = description;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PictureContent that = (PictureContent) o;
-        return Objects.equals(name, that.name) &&
-                Objects.equals(url, that.url) &&
-                Objects.equals(description, that.description);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(name, url, description);
-    }
-
-    @Override
-    public String toString() {
-        return "PictureContent{" +
-                "name='" + name + '\'' +
-                ", url='" + url + '\'' +
-                ", description='" + description + '\'' +
-                '}';
     }
 }
