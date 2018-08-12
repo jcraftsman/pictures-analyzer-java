4627ea67e4338776109c0594a22a9dbda3cf73b7
Wassel Alazhar <wassel.alazhar@gmail.com>
2018-08-12 11:13:02 +0200

    implement local files finder.listFiles

--- src/main/java/org/evolutionary/Finder.java
+++ src/main/java/org/evolutionary/Finder.java
@@ -1,7 +1,26 @@
 package org.evolutionary;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
 public class Finder {
-    public Iterable<File> listFiles(String picturesDirectoryPath) {
-        throw new UnsupportedOperationException();
+    public Iterable<File> listFiles(String directoryPath) {
+        try {
+            return Files.list(Paths.get(directoryPath))
+                    .map(this::mapPathToFile)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    private File mapPathToFile(Path path) {
+        return File.builder()
+                .name(path.getFileName().toString())
+                .path(path.toAbsolutePath().toString())
+                .build();
     }
 }
---
+++ src/test/resources/files/Readme.md
@@ -0,0 +1,3 @@
+# This file is used for test purposes
+
+All temporary directories and files are created based on this directory.
