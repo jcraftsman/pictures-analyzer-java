8e30b585e7f620930f6fff9a10daccaf6f2f078d
Wassel Alazhar <wassel.alazhar@gmail.com>
2018-08-12 19:45:34 +0200

    implement ocr based on tesseract (add tess4j dependency to gradle build) => Acceptance test is green!

--- src/main/java/org/evolutionary/OpticalCharacterRecognition.java
+++ src/main/java/org/evolutionary/OpticalCharacterRecognition.java
@@ -1,7 +1,25 @@
 package org.evolutionary;
 
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import java.io.File;
+
 public class OpticalCharacterRecognition {
+
+    private final ITesseract tesseractInstance;
+
+    public OpticalCharacterRecognition(ITesseract tesseractInstance) {
+        this.tesseractInstance = tesseractInstance;
+    }
+
     public String imageToText(String pathToPictureFile) {
-        throw new UnsupportedOperationException();
+        java.io.File imageFile = new File(pathToPictureFile);
+        try {
+            String result = tesseractInstance.doOCR(imageFile);
+            return result.trim();
+        } catch (TesseractException e) {
+            throw new UnsupportedOperationException();
+        }
     }
 }
